23e17edac3bbb6ca53819cb9e473419883df76e4
ckdrkt2 <bbiuyy@gmail.com>
2021-08-09 23:31:35 +0900

    edting crawler

--- request.py
+++ request.py
@@ -1,15 +1,20 @@
 import requests
 import pandas as pd
-from datetime import datetime
+from datetime import datetime, timedelta
+
+
+def get_response(page,merchantNo, productNo):
+    requests_url = 'https://smartstore.naver.com/i/v1/reviews/paged-reviews?page={0}&pageSize=20&merchantNo={1}&originProductNo={2}&sortType=REVIEW_CREATE_DATE_DESC'.format(page, merchantNo, productNo)
+    return requests.get(requests_url)
 
 def crawl(url, date):
 
     url_elements = url.split('/')
-
+    # 상품번호
     productNo = url_elements[-1]
 
+    # 판매자 번호 & origin상품번호 추출
     response = requests.get(url, stream=True)
-
     merchantNo = ""; ex = ""; script_bit = False; id_bit = False
     for chunk in response.iter_content(chunk_size=512):
         current = ex + chunk.decode('utf-8', errors="ignore")
@@ -26,75 +31,80 @@ def crawl(url, date):
             break
         ex = current[-30:]
     
-    requests_url = 'https://smartstore.naver.com/i/v1/reviews/paged-reviews?page=1&pageSize=20&merchantNo={0}&originProductNo={1}&sortType=REVIEW_CREATE_DATE_DESC'.format(merchantNo, productNo)
-    response = requests.get(requests_url)
-    
+
+    # 페이지 수와 리뷰수 추출
+    response = get_response(1, merchantNo, productNo)
     # 리뷰가 없는 경우 패스
     if response.text == 'OK':
-        return
+        return False
 
     json = response.json()
+    # 범위에 해당하는 데이터가 없을 경우 패스
+    if json['contents'][0]['createDate'][:10] < date:
+        return False
 
     total_page = json['totalPages']
     total_reviews = json['totalElements']
 
+
     # 페이지 체크
     while True:
         try:
-            requests_url = requests_url = 'https://smartstore.naver.com/i/v1/reviews/paged-reviews?page={0}&pageSize=20&merchantNo={1}&originProductNo={2}&sortType=REVIEW_CREATE_DATE_DESC'.format(total_page, merchantNo, productNo)
-            response = requests.get(requests_url)
+            response = get_response(total_page, merchantNo, productNo)
             json = response.json()
             break
-        except:
-            print(requests_url)
-            print(response.text)
+        except: # 총 페이지와 실제 페이지 사이에 차이가 있을 경우 차이를 감소
+            print("delay error", url)
+            total_page -= 1
+
+    # 날짜 조정
+    date = datetime.strptime(date, '%Y-%m-%d')
+    day = timedelta(1)
+    date = str(date-day)[:10]
 
     # 전체 데이터가 기준일 안에 포함될 경우
     if json['contents'][-1]['createDate'][:10] < date:
 
-        # 페이지 찾기
+        # 시작해야할 페이지 찾기
         bot, top = 1, total_page
-
+        
         while True:
-            current_page = (bot + top) // 2
-
-            requests_url = requests_url = 'https://smartstore.naver.com/i/v1/reviews/paged-reviews?page={0}&pageSize=20&merchantNo={1}&originProductNo={2}&sortType=REVIEW_CREATE_DATE_DESC'.format(current_page, merchantNo, productNo)
-            response = requests.get(requests_url)
+            date_page = (bot + top) // 2
+            
+            response = get_response(date_page, merchantNo, productNo)
             json = response.json()
 
             highest_date = json['contents'][0]['createDate'][:10] 
             lowest_date = json['contents'][-1]['createDate'][:10]
-
-            # print(current_page, lowest_date, date, highest_date)
-
-            if lowest_date <= date <= highest_date:
+            
+            if lowest_date <= date <= highest_date or bot == date_page:
                 break
             elif date > highest_date:
-                top = current_page
+                top = date_page
             elif date < lowest_date:
-                bot = current_page
+                bot = date_page            
 
-    # 전체 데이터 중에 기준일이 포함되는 경우
+    # 전체 데이터가 기준일에 포함되는 경우
     else:
-        current_page = total_page
+        date_page = total_page   # 전체 데이터 추출
 
+
+    # 리뷰 추출
     index = 0
-    df = pd.DataFrame(columns=['리뷰ID', '작성자', '작성일', '평점', '상품옵션', '리뷰내용', '좋아요', '이미지'])
-
+    df = pd.DataFrame(columns=['리뷰ID', '작성자', '작성일', '평점', '상품옵션', '리뷰내용', '좋아요', '이미지', '비디오'])
     today = datetime.now().strftime('%Y-%m-%d')
-    page = current_page
-    addreview = False
+    page = date_page
     while page > 0:
 
-        print("running page:", current_page - page + 1, "/", current_page)
+        # 현재 페이지 확인
+        print("running page:", date_page - page + 1, "/", date_page)
 
-        requests_url = 'https://smartstore.naver.com/i/v1/reviews/paged-reviews?page={0}&pageSize=20&merchantNo={1}&originProductNo={2}&sortType=REVIEW_CREATE_DATE_DESC'.format(page, merchantNo, productNo)
-
-        json = requests.get(requests_url).json()
+        json = get_response(page, merchantNo, productNo).json()
+        
         if json['totalElements'] > total_reviews:
-            page += ((json['totalElements'] - total_reviews - 1) // 20) + 1
+            if page < json['totalPages']:
+                page += ((json['totalElements'] - total_reviews - 1) // 20) + 1
             total_reviews = json['totalElements']
-            addreview = True
             continue
 
         reviews = json['contents']
@@ -110,11 +120,11 @@ def crawl(url, date):
             user_id = reviews[i]['writerMemberId']
 
             star = reviews[i]['reviewScore']
+            print(type(star))
 
             try:
                 options = reviews[i]['productOptionContent']
-            except:
-                options = ""
+            except: options = ""
 
             contents = reviews[i]['reviewContent']
 
@@ -122,34 +132,30 @@ def crawl(url, date):
                 like = reviews[i]['helpCount']
             except: like = 0
 
-            img_url = []
+            img_list = []
             if reviews[i]['reviewAttaches']:
                 for attach in reviews[i]['reviewAttaches']:
-                    img_url.append(attach['attachUrl'])
-            if not img_url: img_url = ""
+                    img_list.append(attach['attachUrl'])
+            img_url = " | ".join(img_list)
 
-            df.loc[index] = [reviews[i]['id'], user_id, createDate, star, options, contents, like, img_url]
+            if reviews[i]['reviewAttaches']:
+                for attach in reviews[i]['reviewAttaches']:
+                    
+
+            # dataframe에 저장
+            df.loc[index] = [reviews[i]['id'], user_id, createDate, star, options, contents, like, img_url, video_url]
             
-
             index += 1
         page -= 1
 
     df.to_csv('request/{0}.csv'.format(url_elements[-1]), encoding='utf-8-sig', mode='w')
 
-    return addreview
+    return True
 
 if __name__ == "__main__":
 
-    # url = "https://smartstore.naver.com/ssg01/products/4608892138"
-    # url = "https://smartstore.naver.com/lilydress/products/709465134"
-    # url = "https://smartstore.naver.com/ks1st/products/4497145782"
-    url = "https://smartstore.naver.com/rankingdak/products/521363595"
-    
-    date = '2021-07-20'
+    date = '2020-08-09'
 
-    # url = pd.read_csv("moo.csv")
-    # url = list(url['0'])
-    # for i in url:
-    #     crawl(i, date)
+    url = "https://smartstore.naver.com/lilydress/products/709465134"
 
     crawl(url, date)
