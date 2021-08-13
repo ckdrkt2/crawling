667e387f5374d2041ecd94d70dcaf633b01e71bb
ckdrkt2 <bbiuyy@gmail.com>
2021-08-13 23:48:09 +0900

    crawler update

--- ss_crawler.py
+++ ss_crawler.py
@@ -3,7 +3,8 @@ import selenium
 import pandas as pd
 import time
 import re
-from tqdm import tqdm
+from bs4 import BeautifulSoup
+
 
 def crawl(url):
 
@@ -14,7 +15,7 @@ def crawl(url):
 
     chrome_options = webdriver.ChromeOptions()
     chrome_options.add_argument("--start-maximized")
-    chrome_options.add_argument('headless')
+    # chrome_options.add_argument('headless')
     chrome_options.add_experimental_option('excludeSwitches', ['enable-logging'])
 
     driver = webdriver.Chrome(options=chrome_options)
@@ -52,50 +53,33 @@ def crawl(url):
     
     # 리뷰 추출 부분
     count = reviews // 20 + 1
-    index = 0
-    for page in tqdm(range(1, count+1)):
-        
-        # 페이지 넘기기
-        compare = driver.find_element_by_id('REVIEW').text
-        if page == 1: pass
-        elif page % 10 == 1:
-            page_element = driver.find_element_by_id('REVIEW').find_element_by_link_text('다음')
-            driver.execute_script("arguments[0].scrollIntoView(true);arguments[0].click();", page_element)
-        else:
-            page_element = driver.find_element_by_link_text('{0}'.format(str(page)))
-            driver.execute_script("arguments[0].scrollIntoView(true);arguments[0].click();", page_element)
-        stack = 0
-        if page != 1:
-            while compare == driver.find_element_by_id('REVIEW').text:
-                stack += 1
-                if stack > 100:
-                    print("turn page error:", product_No)
-                pass
-        
+    index, page = 0, 1
+    while page < count+1:
+
+        print(page)
+
+        REVIEW = driver.find_element_by_id('REVIEW')
+    
         # 리뷰 펼치기(사진)
-        elements = driver.find_elements_by_link_text('리뷰 더보기/접기')
+        elements = REVIEW.find_elements_by_link_text('리뷰 더보기/접기')
         for element in elements:
             while True:
                 try:
                     driver.execute_script("arguments[0].scrollIntoView(true);arguments[0].click();", element)
                     break
-                except selenium.common.exceptions.StaleElementReferenceException:
-                    elements = driver.find_elements_by_link_text('리뷰 더보기/접기')
-                    print('stale error', product_No, page, index)
                 except Exception as ex:
                     print(ex)
-                    return
 
-        ul_tags = driver.find_element_by_class_name('review_list').find_elements_by_tag_name('ul')
+        review_list = BeautifulSoup(driver.find_element_by_class_name('review_list').get_attribute('outerHTML'), 'html.parser')
         ul_tag = ""
+        ul_tags = review_list.findAll('ul')
         for i in ul_tags:
             if '신고' in i.text:
                 ul_tag = i
                 break
-        if ul_tag == "": return 
+        if ul_tag == "": return
 
-        # 필요한 li tag 추출
-        li_tags = ul_tag.find_elements_by_tag_name("li")
+        li_tags = ul_tag.findAll("li")
         li_tag_list = []
         for i in li_tags:
             if '신고' in i.text:
@@ -103,9 +87,9 @@ def crawl(url):
 
         for li_tag in li_tag_list:
 
-            star = li_tag.find_element_by_tag_name("em").text
+            star = li_tag.find('em').text
 
-            button_tags = li_tag.find_elements_by_tag_name("button")
+            button_tags = li_tag.findAll("button")
             if len(button_tags) > 1:
                 options = button_tags[0].text
                 like = button_tags[1].text
@@ -113,7 +97,7 @@ def crawl(url):
                 options = ""
                 like = button_tags[0].text
 
-            user_id = li_tag.find_element_by_tag_name("strong").text
+            user_id = li_tag.find("strong").text
 
             try:
                 date = re.compile(r"\d{2}\.\d{2}\.\d{2}\.").findall(li_tag.text)[0]
@@ -122,16 +106,16 @@ def crawl(url):
                 print(li_tag.text)
                 return
 
-            contents = li_tag.find_element_by_class_name("YEtwtZFLDz").text
+            contents = li_tag.find("div", "YEtwtZFLDz").text
 
             img_list = []
 
-            attaches = li_tag.find_elements_by_tag_name('ul')
+            attaches = li_tag.findAll('ul')
             if attaches:
                 attach = attaches[0]
-                img_tags = attach.find_elements_by_tag_name("img")
+                img_tags = attach.findAll("img")
                 for img_tag in img_tags:
-                    src = img_tag.get_attribute('src').split('?')[0]
+                    src = img_tag['src'].split('?')[0]
                     if 'data' not in src: img_list.append(src)
                 img_url = ' | '.join(img_list)
             else: img_url = ""
@@ -139,6 +123,17 @@ def crawl(url):
             df.loc[index] = [user_id, date, star, options, contents, like, img_url]
 
             index += 1
+        
+        page += 1
+        
+        # 페이지 넘기기
+        if page % 10 == 1:
+            page_element = REVIEW.find_element_by_link_text('다음')
+            driver.execute_script("arguments[0].scrollIntoView(true);arguments[0].click();", page_element)
+        else:
+            page_element = REVIEW.find_element_by_link_text('{0}'.format(str(page)))
+            driver.execute_script("arguments[0].scrollIntoView(true);arguments[0].click();", page_element)
+        
 
     df.to_csv('test/{0}.csv'.format(product_No), encoding='utf-8-sig', mode='w')
 
@@ -148,6 +143,6 @@ if __name__ == "__main__":
 
     start = time.time()
     
-    crawl("https://smartstore.naver.com/lilydress/products/690142213")
+    crawl("https://smartstore.naver.com/lilydress/products/3895706314")
 
     print("total run time:", time.time() - start)
